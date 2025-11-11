b91762a744ebb2d928d31ca7c5be9f244f5f207e
saud khan <asaud3508@gmail.com>
2025-11-11 22:22:07 +0500

    controller for Course is Done

--- Qec_Project.Api/controllers/CourseController.cs
+++ Qec_Project.Api/controllers/CourseController.cs
@@ -1,6 +1,6 @@
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Qec_Project.Api.model;
+using Qec_Project.Api.Repository;
 
 namespace QEC_Project.API.controllers
 {
@@ -8,16 +8,77 @@ namespace QEC_Project.API.controllers
     [ApiController]
     public class CourseController : ControllerBase
     {
-        private readonly DBContext _dBContext;
-        public CourseController(DBContext dBContext)
+        private CourseRepository _courseRepository;
+        public CourseController(CourseRepository courseRepository)
         {
-            this._dBContext = dBContext;
+            this._courseRepository = courseRepository;
         }
 
         [HttpGet]
-    public  async Task<List<CourseModel>> GgetCourses()
-    {
-            return await _dBContext.Course.ToListAsync();
+        public async Task<List<CourseModel>> GgetCourses()
+        {
+            return await this._courseRepository.GetAllCourse();
+        }
+
+        [HttpPost("create-course")]
+        public async Task<IActionResult> CreateCourse(CourseModel courseModel)
+        {
+            var res = await this._courseRepository.CreateCourse(courseModel);
+            if (!res.success)
+            {
+                ModelState.AddModelError("Error", res.message);
+                var validation = new ValidationProblemDetails(ModelState);
+                return BadRequest(validation);
+            }
+            return Ok(new ResponseDTO(res.success, res.message));
+
+        }
+
+        [HttpGet("{courseCode}")]
+        public async Task<IActionResult> GetSpecificCourse(string courseCode)
+        {
+            var res = await this._courseRepository.GetSpecificCourse(courseCode);
+            if (res == null)
+            {
+                ModelState.AddModelError("Error", "Course not found");
+                var validation = new ValidationProblemDetails(ModelState);
+                return BadRequest(validation);
+            }
+
+            return Ok(new CourseDTO(res.CourseCode, res.Description, res.TheoreyCreditHours,
+                res.LabCreditHours, res.IsActive));
+
+        }
+
+        [HttpPut("{courseCode}")]
+        public async Task<IActionResult> UpdateCourse(CourseModel courseModel)
+        {
+            var res = await this._courseRepository.UpdateCourse(courseModel);
+            if (!res.success)
+            {
+                ModelState.AddModelError("Error", res.message);
+                var validation = new ValidationProblemDetails(ModelState);
+                return BadRequest(validation);
+
+            }
+            return Ok(new ResponseDTO(res.success, res.message));
+        }
+
+        [HttpDelete("{courseCode}")]
+        public async Task<IActionResult> DeleteCourse(string courseCode)
+        {
+            var res = await this._courseRepository.DeleteCourse(courseCode);
+            if (!res.success)
+            {
+                ModelState.AddModelError("Error", res.message);
+                var validation = new ValidationProblemDetails(ModelState);
+                return BadRequest(validation);
+
+            }
+            return Ok(new ResponseDTO(res.success, res.message));
+        }
+
+
+
     }
-  }
 }
